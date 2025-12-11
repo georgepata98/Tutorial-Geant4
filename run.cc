cf441949208bde1cb3ea381f82234a522cd86b09
George PATA <gpata11@yahoo.com>
2025-12-11 08:26:42 +0200

    Am updatat tutorialul adaugand si fisierele de data analysis, adica run.hh si run.cc si modificat cele cu detector

---
+++ run.cc
@@ -0,0 +1,29 @@
+#include "run.hh"
+
+MyRunAction::MyRunAction()
+{}
+
+MyRunAction::~MyRunAction()
+{}
+
+void MyRunAction::BeginOfRunAction(const G4Run*)
+{
+    G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+    man->OpenFile("output.root");
+
+    man->CreateNtuple("Hits", "Hits");
+    man->CreateNtupleIColumn("fEvent");  // event number ca input
+    man->CreateNtupleDColumn("fX");
+    man->CreateNtupleDColumn("fY");
+    man->CreateNtupleDColumn("fZ");
+    man->FinishNtuple(0);  // ntuple number 0
+}
+
+void MyRunAction::EndOfRunAction(const G4Run*)
+{
+    G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+    man->Write();
+    man->CloseFile();
+}
